d537bb4d85d95505534982b8b3332e84770db74d
jithin-sunil <jithinsunil2003@gmail.com>
2026-03-21 00:51:24 +0530

    ForgotPassword and Email , ChatBot

--- Basics/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from Basics import views
-
-urlpatterns = [
-    path('Sum/',views.Sum,name="Sum"),
-    path('Large/',views.Large,name="Large"),
-    path('Calc/',views.Calc,name="Calc"),
-]
